3e95c49e52c13a6bbb938dc03320ff6a4ba90287
ChawEiPhyu308 <chaweiphyu@my.jcu.edu.au>
2020-09-16 13:00:48 +0000

    Fixing atttempt here we go

--- pull.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-
-if ($_POST['payload']) {
-    shell_exec('cd && cd www/chaweip.sgedu.site/public_html/a2-staging/ && git pull');
-}
-
-?>hi
