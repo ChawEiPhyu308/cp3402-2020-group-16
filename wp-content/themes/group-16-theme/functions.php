23b0bd827892f34a8f89d6397ca3b7759aa0486b
ChawEiPhyu308 <chaweiphyu@my.jcu.edu.au>
2020-09-18 20:11:31 +0800

    Stylesheet change

--- wp-content/themes/group-16-theme/functions.php
+++ wp-content/themes/group-16-theme/functions.php
@@ -154,8 +154,6 @@ add_action( 'wp_enqueue_scripts', 'group_16_theme_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
-
 /**
  * Custom template tags for this theme.
  */
