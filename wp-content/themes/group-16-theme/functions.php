bd27f4872ae405eaa4514f43ef03f86b75ba6f56
ChawEiPhyu308 <chaweiphyu@my.jcu.edu.au>
2020-09-18 20:13:25 +0800

    Stylesheet push

--- wp-content/themes/group-16-theme/functions.php
+++ wp-content/themes/group-16-theme/functions.php
@@ -151,27 +151,7 @@ function group_16_theme_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'group_16_theme_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
 
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
